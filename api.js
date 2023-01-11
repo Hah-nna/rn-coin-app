d6d198a031f4dc74b833ec13d52e79fdf453f650
Jeong <hj08hn@gmail.com>
2023-01-11 14:52:40 +0900

    fix address
    -json 배포 주소 바꿈
    
    @Jeon hana

--- api.js
+++ api.js
@@ -22,31 +22,33 @@ export const searchCoin = () => {
 // CRUD
 
 export const getPost = async () => {
-  return await axios.get("https://outgoing-heartbreaking-baron.glitch.me");
+  return await axios.get(
+    "https://outgoing-heartbreaking-baron.glitch.me/posts"
+  );
   // const getPostData = await axios.get("http://192.168.200.115:3001/posts");
 
   /**
    * json 서버를 열 때 본인의 ip주소를 맞추어야한다
    * json 서버를 열 때 아래와 같이 입력한다
-   * yarn json-server --watch db.json --port 3001 --host 192.168.35.126(192~부터는 자신의 ip주소임)
+   * yarn json-server --watch db.json --port 3001 --host 192.168.200.115(192~부터는 자신의 ip주소임)
    * api.js에서 자신의 ip주소로 바꾸세요
    */
 };
 
 export const createPost = async (item) => {
   return await axios.post(
-    "https://outgoing-heartbreaking-baron.glitch.me",
+    "https://outgoing-heartbreaking-baron.glitch.me/posts",
     item
   );
 };
 export const updatePost = async (item) => {
   return await axios.patch(
-    `https://outgoing-heartbreaking-baron.glitch.me/${item.id}`,
+    `https://outgoing-heartbreaking-baron.glitch.me/posts${item.id}`,
     item
   );
 };
 export const removePost = async (item) => {
   return await axios.delete(
-    `https://outgoing-heartbreaking-baron.glitch.me/${item.id}`
+    `https://outgoing-heartbreaking-baron.glitch.me/posts${item.id}`
   );
 };
