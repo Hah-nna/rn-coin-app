d63ea63365d05c932ba6e4a66affa2f06b1ee38c
hwuiinn <gnldls8406@gmail.com>
2023-01-11 10:56:51 +0900

    :bug: (coinPriceSlice.js) Func fix
    - 기존 IOS에서만 1000단위로 쉼표가 적용되었던 것을, 안드로이드와 IOS에 적용되게 코드를 수정하였음.
    
    - before : Math.trunc(price).toLocaleString();
    
    - after :
     .toString()
        .replace(/\B(?=(\d{3})+(?!\d))/g, ",");

--- util/coinPriceSlice.js
+++ util/coinPriceSlice.js
@@ -1,5 +1,7 @@
 const coinPriceSlice = (price) => {
-  return Math.trunc(price).toLocaleString();
+  return Math.trunc(price)
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
 
 export default coinPriceSlice;
